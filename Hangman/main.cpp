a5e90303af2136a425e573915e07765bbfbd09a4
Dóra Kinorányi <42067569+dorakino@users.noreply.github.com>
2021-03-23 15:31:34 +0100

    Create main.cpp

---
+++ Hangman/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <fstream>
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+#include "Hangman.h"
+
+
+int main()
+{
+    Hangman p;
+    p.play();
+    
+    return 0;
+}
